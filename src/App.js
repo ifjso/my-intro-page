bd604942bc7b54b78dd265c3546799c7e2175392
JesusOh <ojsboy@hotmail.com>
2018-11-10 12:01:21 +0900

    Resolve conflict of eslint

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-<<<<<<< HEAD
     render() {
         return (
             <div className="App">
@@ -24,49 +23,6 @@ class App extends Component {
             </div>
         );
     }
-||||||| merged common ancestors
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-=======
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
-        );
-    }
->>>>>>> b4c50f2d1ad8cf202f50b7ce612d09c024661141
 }
 
 export default App;
