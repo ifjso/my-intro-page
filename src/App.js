6c899446031a3b9fbf44c670656cf275bc8bfaaa
JesusOh <ojsboy@hotmail.com>
2018-11-12 06:53:29 +0900

    Test Donut chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AOS from 'aos';
+import "aos/dist/aos.css";
+import './App.scss';
+import './DonutChart.scss';
 
 class App extends Component {
+    componentDidMount() {
+        AOS.init({
+            once: true,
+            easing: "ease-in-out-sine"
+        });
+
+        document.addEventListener('aos:in:graph-2', ({ detail }) => {
+            console.log('animated in', detail);
+        });
+    }
+
     render() {
+        const graphDuration = 1500;
+        
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-            Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-            Learn React
-                    </a>
-                </header>
+                <div className="wrap-graph">
+                    <div className="graph2 bg-graph" 
+                        data-aos="rotate-graph"
+                        data-aos-duration={graphDuration}></div>
+                    <div className="graph2 bg-graph" 
+                        data-aos="rotate-graph"
+                        data-aos-duration={graphDuration}
+                        data-aos-delay={graphDuration}></div>
+                    <div className="graph2 bg-graph" 
+                        data-aos="rotate-graph"
+                        data-aos-duration={graphDuration}
+                        data-aos-delay={graphDuration * 2}
+                        data-aos-id="graph-2"></div>
+                    {/* <div className="jelly" data-aos="donut"></div> */}
+                    <div className="don" data-aos="don-t"></div>
+                </div>
             </div>
         );
     }
