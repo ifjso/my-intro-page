5d7cbaa8e0578145daac1d521dcbf99195ac9df5
Dval <ojsboy@hotmail.com>
2018-11-15 14:54:20 +0900

    Complete today intro

---
+++ src/components/TodayIntro/index.js
@@ -0,0 +1,19 @@
+/* --------------------------------------------------------
+author: Dval
+----------------------------------------------------------*/
+
+import React from 'react';
+import './TodayIntro.scss';
+
+const TodayIntro = () =>
+    <div className="today-intro">
+        <p className="align-center">
+            현명한 소비습관,<br/>
+            하루 한번 <strong>{"'오늘'"}</strong> 하세요!
+        </p>
+        <div className="wrap-today-phone">
+            <div className="today-phone"></div>
+        </div>
+    </div>;
+
+export default TodayIntro;    
