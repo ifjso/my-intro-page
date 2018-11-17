dc6c0a6d75a805f693de07e6668fcf36d6f06932
JesusOh <ojsboy@hotmail.com>
2018-11-17 20:47:23 +0900

    Add OneDayIntro text

--- src/components/OneDayIntro/index.js
+++ src/components/OneDayIntro/index.js
@@ -7,8 +7,13 @@ import Box from '../Box';
 import OneDayCoins from '../OneDayCoins';
 import './OneDayIntro.scss';
 
-const OneDayIntro = () => 
+const OneDayIntro = () =>
     <div className="one-day-intro">
+        <p className="align-center">
+            <strong>덜</strong> 쓰면 늘어나고, <strong>더</strong> 쓰면 줄어드는<br/>
+            <strong>하루 예산</strong>을 보며<br/>
+            매일 지출을 관리하세요.
+        </p>
         <Box id="one-day-intro-box" aosDelay={1800} />
         <OneDayCoins />
     </div>;
