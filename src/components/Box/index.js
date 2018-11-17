8c3de0673e332619a2c1506795e07b7a38ed9cff
JesusOh <ojsboy@hotmail.com>
2018-11-17 20:14:46 +0900

    Add Box component

---
+++ src/components/Box/index.js
@@ -0,0 +1,27 @@
+/* --------------------------------------------------------
+author: Dval
+----------------------------------------------------------*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Box.scss';
+
+const Box = ({id, aosType = 'one-day-intro-box-slide-down', aosDelay = 0 }) => {
+    const boxAosOpts = {
+        'data-aos': aosType,
+        'data-aos-delay': aosDelay,
+        'data-aos-anchor-placement': 'top-bottom',
+    };
+
+    return (
+        <div className="one-day-intro-box" id={id} {...boxAosOpts} />
+    );
+};
+
+Box.propTypes = {
+    id: PropTypes.string,
+    aosType: PropTypes.string,
+    aosDelay: PropTypes.number
+};
+
+export default Box;
--- src/components/OneDayIntro/index.js
+++ src/components/OneDayIntro/index.js
@@ -3,66 +3,58 @@ author: Dval
 ----------------------------------------------------------*/
 
 import React from 'react';
+import Box from '../Box';
 import CoinBox, { CoinBoxType } from '../CoinBox';
 import './OneDayIntro.scss';
 
-const OneDayIntro = () => {
-    const boxAosOpts = {
-        'data-aos-anchor-placement': 'top-bottom',
-        'data-aos': 'one-day-intro-box-slide-down',
-        'data-aos-delay': 1800
-    };
+const OneDayIntro = () => 
+    <div className="one-day-intro">
+        <Box id="one-day-intro-box" aosDelay={1800} />
 
-    return (
-        <div className="one-day-intro">
-            <div className="one-day-intro-box" id="one-day-intro-box" {...boxAosOpts} />
-
-            <div className="wrap-coin" style={{top: 46}}>
-                <CoinBox calcTop={calcTop(5)}
-                    useBoxAos={false}
-                    useOuterCoinAos={true}
-                    outerCoinAosType="coin-slide-right"
-                />
-                <CoinBox count={4}
-                    calcTop={calcTop(4)}
-                    boxAosType="coin-box-slide-down"
-                    boxAosDelay={1000}
-                    useInnerCoinAos={true}
-                    calcInnerCoinAosDelay={calcDelay(3)}
-                />
-                <CoinBox type={CoinBoxType.COIN_BOX_RIGHT}
-                    count={5}
-                    calcTop={calcTop(5)}
-                    boxAosDelay={1000}
-                />
-            </div>
-
-            <div className="wrap-coin" style={{top: 108}}>
-                <CoinBox type={CoinBoxType.COIN_BOX_RIGHT}
-                    calcTop={calcTop(5)}
-                    useBoxAos={false}
-                    useOuterCoinAos={true}
-                    useInnerCoinAos={true}
-                    outerCoinAosDelay={3700}
-                    calcInnerCoinAosDelay={calcDelay(6, bottomCoinBoxAosDelayOffset)}
-                    innerCoinAosDuration={300}
-                />
-                <CoinBox count={5}
-                    calcTop={calcTop(5)} 
-                    boxAosDelay={3700}
-                    useInnerCoinAos={true}
-                    calcInnerCoinAosDelay={calcDelay(4, bottomCoinBoxAosDelayOffset)}
-                />
-                <CoinBox type={CoinBoxType.COIN_BOX_RIGHT}
-                    count={4}
-                    calcTop={calcTop(4)}
-                    boxAosType="coin-box-slide-down"
-                    boxAosDelay={3700}
-                />
-            </div>
+        <div className="wrap-coin" style={{top: 46}}>
+            <CoinBox calcTop={calcTop(5)}
+                useBoxAos={false}
+                useOuterCoinAos={true}
+                outerCoinAosType="coin-slide-right"
+            />
+            <CoinBox count={4}
+                calcTop={calcTop(4)}
+                boxAosType="coin-box-slide-down"
+                boxAosDelay={1000}
+                useInnerCoinAos={true}
+                calcInnerCoinAosDelay={calcDelay(3)}
+            />
+            <CoinBox type={CoinBoxType.COIN_BOX_RIGHT}
+                count={5}
+                calcTop={calcTop(5)}
+                boxAosDelay={1000}
+            />
         </div>
-    );
-};
+
+        <div className="wrap-coin" style={{top: 108}}>
+            <CoinBox type={CoinBoxType.COIN_BOX_RIGHT}
+                calcTop={calcTop(5)}
+                useBoxAos={false}
+                useOuterCoinAos={true}
+                useInnerCoinAos={true}
+                outerCoinAosDelay={3700}
+                calcInnerCoinAosDelay={calcDelay(6, bottomCoinBoxAosDelayOffset)}
+                innerCoinAosDuration={300}
+            />
+            <CoinBox count={5}
+                calcTop={calcTop(5)} 
+                boxAosDelay={3700}
+                useInnerCoinAos={true}
+                calcInnerCoinAosDelay={calcDelay(4, bottomCoinBoxAosDelayOffset)}
+            />
+            <CoinBox type={CoinBoxType.COIN_BOX_RIGHT}
+                count={4}
+                calcTop={calcTop(4)}
+                boxAosType="coin-box-slide-down"
+                boxAosDelay={3700}
+            />
+        </div>
+    </div>;
 
 const calcTop = n => i => 17 * (n - i);
 const calcDelay = (n, d = 0) => i => 200 * (n - i) + d;
