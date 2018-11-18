030fe66466a32f01b43372f176388897c942f1b8
JesusOh <ojsboy@hotmail.com>
2018-11-18 10:30:18 +0900

    Change intro height interval

--- src/components/BudgetSettingIntro/index.js
+++ src/components/BudgetSettingIntro/index.js
@@ -8,7 +8,7 @@ import './BudgetSettingIntro.scss';
 
 const BudgetSettingIntro = () =>
     <div className="budget-setting-intro">
-        {/* <p style={{position:'absolute', top: 568, left: 119}}>내 지난 소비를 참고해</p> */}
+        {/* <p style={{position:'absolute', top: 1492, left: 119}}>내 지난 소비를 참고해</p> */}
         <p className="align-center">
             내 지난 소비를 참고해<br/>
             <strong>한달</strong> 또는 <strong>하루 예산</strong>을 정하고,
