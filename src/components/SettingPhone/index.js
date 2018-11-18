4624e0f5c044bbfdc42b256bfb12dde27279e68f
JesusOh <ojsboy@hotmail.com>
2018-11-18 23:14:40 +0900

    Add mobile web trick

--- src/components/SettingPhone/index.js
+++ src/components/SettingPhone/index.js
@@ -27,23 +27,23 @@ const SettingPhone = () => {
     return (
         <div className="setting-phone" id="setting-phone" {...aosOpts}>
             <div className="img-setting-phone">
-                <SettingTextBundle aosType="d-fade-out" aosAnchor="#setting-phone" aosDelay="2500">
+                <SettingTextBundle aosType="d-fade-out" aosAnchor="#setting-phone" aosDelay={2500}>
                     <SettingText text="2018년 9월 12일" />
                     <SettingText text="예산 종료일 2018년 10월 11일" type={TextType.SMALL} top={120} />
                 </SettingTextBundle>
     
-                <SettingTextBundle aosType="fade" aosAnchor="#setting-phone" aosDelay="2500">
+                <SettingTextBundle aosType="fade" aosAnchor="#setting-phone" aosDelay={2500}>
                     <SettingText text="2018년 9월 25일" type={TextType.LARGE} top={93}  />
                     <SettingText text="예산 종료일 2018년 10월 24일" type={TextType.SMALL} top={120} />
                 </SettingTextBundle>
 
-                <SettingTextBundle type={TextBundleType.BOTTOM} aosType="d-fade-out" aosAnchor="#setting-phone" aosDelay="3700">
+                <SettingTextBundle type={TextBundleType.BOTTOM} aosType="d-fade-out" aosAnchor="#setting-phone" aosDelay={3700}>
                     <SettingText id="average-budget-value" text="1,456,000원" top={21} />
                     <SettingText id="average-budget-text" text="지난 소비 기준 한달 평균 예산" type={TextType.SMALL} top={48} />
                     <SettingText text="48,533원" top={82} />
                 </SettingTextBundle>
     
-                <SettingTextBundle type={TextBundleType.BOTTOM} aosType="fade" aosAnchor="#setting-phone" aosDelay="3700">
+                <SettingTextBundle type={TextBundleType.BOTTOM} aosType="fade" aosAnchor="#setting-phone" aosDelay={3700}>
                     <SettingText text="1,500,000원" type={TextType.LARGE} top={20} />
                     <SettingText text="50,000원" type={TextType.LARGE} top={81} />
                 </SettingTextBundle>
