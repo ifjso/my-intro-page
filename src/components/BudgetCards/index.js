ec01351dcef80c6fae1dd502200718699264fc25
JesusOh <ojsboy@hotmail.com>
2018-11-19 00:06:08 +0900

    Remove event

--- src/components/BudgetCards/index.js
+++ src/components/BudgetCards/index.js
@@ -2,48 +2,56 @@
 author: Dval
 ----------------------------------------------------------*/
 
-import React from 'react';
+import React, { Component } from 'react';
 import BudgetCard, { BudgetCardType } from '../BudgetCard';
 import Donut from '../Donut';
 import HalfDonut, { HalfDonutType } from '../HalfDonut';
 import './BudgetCards.scss';
 
-const BudgetCards = () => {
-    // 예산 카드 이벤트 시작 시 도넛 애니메이션도 시작
-    document.addEventListener('aos:in:swipe-card', () => {
-        const halfDonuts = document.getElementsByClassName('half-donut');
-        Array.prototype.forEach.call(halfDonuts, (halfDonut, i) => {
-            halfDonut.classList.add(`half-donut${i}-rotate`);
+class BudgetCards extends Component {
+    componentDidMount() {
+        // 예산 카드 이벤트 시작 시 도넛 애니메이션도 시작
+        document.addEventListener('aos:in:swipe-card', () => {
+            const halfDonuts = document.getElementsByClassName('half-donut');
+            Array.prototype.forEach.call(halfDonuts, (halfDonut, i) => {
+                halfDonut.classList.add(`half-donut${i}-rotate`);
+            });
         });
-    });
+    }
 
-    const commonAosOpts = {
-        'data-aos': 'swipe-card',
-        'data-aos-id': 'swipe-card',
-        'data-aos-easing': 'ease-out-quart',
-        'data-aos-anchor-placement': 'top-bottom',
-        'data-aos-delay': '1200',
-        'data-aos-duration': '900'
-    };
+    componentWillUnmount() {
+        document.removeEventListener('aos:in:swipe-card');
+    }
 
-    return (
-        <div className="wrap-budget-cards" {...commonAosOpts} >
-            <BudgetCard type={BudgetCardType.BUDGET_CARD_0} dateText="2018년 7월 25일 ~ 2018년 8월 24일" />
+    render() {
+        const commonAosOpts = {
+            'data-aos': 'swipe-card',
+            'data-aos-id': 'swipe-card',
+            'data-aos-easing': 'ease-out-quart',
+            'data-aos-anchor-placement': 'top-bottom',
+            'data-aos-delay': '1200',
+            'data-aos-duration': '900'
+        };
 
-            <BudgetCard type={BudgetCardType.BUDGET_CARD_1} dateText="2018년 8월 25일 ~ 2018년 9월 24일">
-                <Donut />
-                <HalfDonut type={HalfDonutType.HALF_DONUT_0} />
-                <HalfDonut type={HalfDonutType.HALF_DONUT_0} left={20} transform="rotate(180deg)" />
-                <HalfDonut type={HalfDonutType.HALF_DONUT_2} />
-            </BudgetCard>
+        return (
+            <div className="wrap-budget-cards" {...commonAosOpts} >
+                <BudgetCard type={BudgetCardType.BUDGET_CARD_0} dateText="2018년 7월 25일 ~ 2018년 8월 24일" />
 
-            <BudgetCard type={BudgetCardType.BUDGET_CARD_2} dateText="2018년 9월 25일 ~ 2018년 10월 24일">
-                <Donut />
-                <HalfDonut type={HalfDonutType.HALF_DONUT_1} />
-                <HalfDonut type={HalfDonutType.HALF_DONUT_0} />
-            </BudgetCard>
-        </div>
-    );
-};
+                <BudgetCard type={BudgetCardType.BUDGET_CARD_1} dateText="2018년 8월 25일 ~ 2018년 9월 24일">
+                    <Donut />
+                    <HalfDonut type={HalfDonutType.HALF_DONUT_0} />
+                    <HalfDonut type={HalfDonutType.HALF_DONUT_0} left={20} transform="rotate(180deg)" />
+                    <HalfDonut type={HalfDonutType.HALF_DONUT_2} />
+                </BudgetCard>
+
+                <BudgetCard type={BudgetCardType.BUDGET_CARD_2} dateText="2018년 9월 25일 ~ 2018년 10월 24일">
+                    <Donut />
+                    <HalfDonut type={HalfDonutType.HALF_DONUT_1} />
+                    <HalfDonut type={HalfDonutType.HALF_DONUT_0} />
+                </BudgetCard>
+            </div>
+        );
+    }
+}
 
 export default BudgetCards;
--- src/components/SettingPhone/index.js
+++ src/components/SettingPhone/index.js
@@ -2,56 +2,63 @@
 author: Dval
 ----------------------------------------------------------*/
 
-import React from 'react';
+import React, { Component } from 'react';
 import SettingText, { SettingTextType as TextType } from '../SettingText';
 import SettingTextStamp from '../SettingTextStamp';
 import SettingTextBundle, { SettingTextBundleType as TextBundleType } from '../SettingTextBundle';
 import './SettingPhone.scss';
 
-const SettingPhone = () => {
-    document.addEventListener('aos:in:setting-phone', () => {
-        const addAnimation = (id, className) => document.getElementById(id).classList.add(className);
-        addAnimation('average-budget-stamp', 'blink');
-        addAnimation('average-budget-text', 'blink-back');
-        addAnimation('average-budget-value', 'blink-orange');
-    });
+class SettingPhone extends Component {
+    componentDidMount() {
+        document.addEventListener('aos:in:setting-phone', () => {
+            const addAnimation = (id, className) => document.getElementById(id).classList.add(className);
+            addAnimation('average-budget-stamp', 'blink');
+            addAnimation('average-budget-text', 'blink-back');
+            addAnimation('average-budget-value', 'blink-orange');
+        });
+    }
 
-    const aosOpts = {
-        'data-aos': 'fade-up',
-        'data-aos-id': 'setting-phone',
-        'data-aos-easing': 'ease-in-sine',
-        'data-aos-duration': 600,
-        'data-aos-anchor-placement': 'top-bottom'
-    };
+    componentWillUnmount() {
+        document.removeEventListener('aos:in:setting-phone');
+    }
 
-    return (
-        <div className="setting-phone" id="setting-phone" {...aosOpts}>
-            <div className="img-setting-phone">
-                <SettingTextBundle aosType="d-fade-out" aosAnchor="#setting-phone" aosDelay={2500}>
-                    <SettingText text="2018년 9월 12일" />
-                    <SettingText text="예산 종료일 2018년 10월 11일" type={TextType.SMALL} top={120} />
-                </SettingTextBundle>
-    
-                <SettingTextBundle aosType="fade" aosAnchor="#setting-phone" aosDelay={2500}>
-                    <SettingText text="2018년 9월 25일" type={TextType.LARGE} top={93}  />
-                    <SettingText text="예산 종료일 2018년 10월 24일" type={TextType.SMALL} top={120} />
-                </SettingTextBundle>
-
-                <SettingTextBundle type={TextBundleType.BOTTOM} aosType="d-fade-out" aosAnchor="#setting-phone" aosDelay={3700}>
-                    <SettingText id="average-budget-value" text="1,456,000원" top={21} />
-                    <SettingText id="average-budget-text" text="지난 소비 기준 한달 평균 예산" type={TextType.SMALL} top={48} />
-                    <SettingText text="48,533원" top={82} />
-                </SettingTextBundle>
-    
-                <SettingTextBundle type={TextBundleType.BOTTOM} aosType="fade" aosAnchor="#setting-phone" aosDelay={3700}>
-                    <SettingText text="1,500,000원" type={TextType.LARGE} top={20} />
-                    <SettingText text="50,000원" type={TextType.LARGE} top={81} />
-                </SettingTextBundle>
-    
-                <SettingTextStamp id="average-budget-stamp" text="지난 소비 기준 한달 평균 예산" />
+    render() {
+        const aosOpts = {
+            'data-aos': 'fade-up',
+            'data-aos-id': 'setting-phone',
+            'data-aos-easing': 'ease-in-sine',
+            'data-aos-duration': 600,
+            'data-aos-anchor-placement': 'top-bottom'
+        };
+        return (
+            <div className="setting-phone" id="setting-phone" {...aosOpts}>
+                <div className="img-setting-phone">
+                    <SettingTextBundle aosType="d-fade-out" aosAnchor="#setting-phone" aosDelay={2500}>
+                        <SettingText text="2018년 9월 12일" />
+                        <SettingText text="예산 종료일 2018년 10월 11일" type={TextType.SMALL} top={120} />
+                    </SettingTextBundle>
+            
+                    <SettingTextBundle aosType="fade" aosAnchor="#setting-phone" aosDelay={2500}>
+                        <SettingText text="2018년 9월 25일" type={TextType.LARGE} top={93}  />
+                        <SettingText text="예산 종료일 2018년 10월 24일" type={TextType.SMALL} top={120} />
+                    </SettingTextBundle>
+            
+                    <SettingTextBundle type={TextBundleType.BOTTOM} aosType="d-fade-out" aosAnchor="#setting-phone" aosDelay={3700}>
+                        <SettingText id="average-budget-value" text="1,456,000원" top={21} />
+                        <SettingText id="average-budget-text" text="지난 소비 기준 한달 평균 예산" type={TextType.SMALL} top={48} />
+                        <SettingText text="48,533원" top={82} />
+                    </SettingTextBundle>
+            
+                    <SettingTextBundle type={TextBundleType.BOTTOM} aosType="fade" aosAnchor="#setting-phone" aosDelay={3700}>
+                        <SettingText text="1,500,000원" type={TextType.LARGE} top={20} />
+                        <SettingText text="50,000원" type={TextType.LARGE} top={81} />
+                    </SettingTextBundle>
+            
+                    <SettingTextStamp id="average-budget-stamp" text="지난 소비 기준 한달 평균 예산" />
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
     
 export default SettingPhone;
