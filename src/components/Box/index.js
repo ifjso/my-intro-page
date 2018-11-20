0a0ed24e94ebff9df789bc85899bdd4353387fd7
JesusOh <ojsboy@hotmail.com>
2018-11-20 20:56:48 +0900

    Fix conflicts

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-/* --------------------------------------------------------
-author: Dval
-----------------------------------------------------------*/
-
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Box.scss';
-
-const Box = ({id, aosType = 'one-day-intro-box-slide-down', aosDelay = 0 }) => {
-    const boxAosOpts = {
-        'data-aos': aosType,
-        'data-aos-delay': aosDelay,
-        'data-aos-anchor-placement': 'top-bottom',
-    };
-
-    return (
-        <div className="one-day-intro-box" id={id} {...boxAosOpts} />
-    );
-};
-
-Box.propTypes = {
-    id: PropTypes.string,
-    aosType: PropTypes.string,
-    aosDelay: PropTypes.number
-};
-
-export default Box;
-||||||| merged common ancestors
-=======
 /* --------------------------------------------------------
 author: Dval
 ----------------------------------------------------------*/
@@ -56,4 +26,3 @@ Box.propTypes = {
 };
 
 export default Box;
->>>>>>> cdffb6465432010e44806591f874f82fa731e8f1
--- src/components/BudgetCards/index.js
+++ src/components/BudgetCards/index.js
@@ -1,63 +1,3 @@
-<<<<<<< HEAD
-/* --------------------------------------------------------
-author: Dval
-----------------------------------------------------------*/
-
-import React, { Component } from 'react';
-import BudgetCard, { BudgetCardType } from '../BudgetCard';
-import Donut from '../Donut';
-import HalfDonut, { HalfDonutType } from '../HalfDonut';
-import './BudgetCards.scss';
-
-class BudgetCards extends Component {
-    componentDidMount() {
-        // 예산 카드 이벤트 시작 시 도넛 애니메이션도 시작
-        document.addEventListener('aos:in:swipe-card', () => {
-            const halfDonuts = document.getElementsByClassName('half-donut');
-            Array.prototype.forEach.call(halfDonuts, (halfDonut, i) => {
-                halfDonut.classList.add(`half-donut${i}-rotate`);
-            });
-        });
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('aos:in:swipe-card');
-    }
-
-    render() {
-        const commonAosOpts = {
-            'data-aos': 'swipe-card',
-            'data-aos-id': 'swipe-card',
-            'data-aos-easing': 'ease',
-            'data-aos-anchor-placement': 'top-bottom',
-            'data-aos-delay': '1200',
-            'data-aos-duration': '900'
-        };
-
-        return (
-            <div className="wrap-budget-cards" {...commonAosOpts} >
-                <BudgetCard type={BudgetCardType.BUDGET_CARD_0} dateText="2018년 7월 25일 ~ 2018년 8월 24일" />
-
-                <BudgetCard type={BudgetCardType.BUDGET_CARD_1} dateText="2018년 8월 25일 ~ 2018년 9월 24일">
-                    <Donut />
-                    <HalfDonut type={HalfDonutType.HALF_DONUT_0} />
-                    <HalfDonut type={HalfDonutType.HALF_DONUT_0} left={20} transform="rotate(180deg)" />
-                    <HalfDonut type={HalfDonutType.HALF_DONUT_2} />
-                </BudgetCard>
-
-                <BudgetCard type={BudgetCardType.BUDGET_CARD_2} dateText="2018년 9월 25일 ~ 2018년 10월 24일">
-                    <Donut />
-                    <HalfDonut type={HalfDonutType.HALF_DONUT_1} />
-                    <HalfDonut type={HalfDonutType.HALF_DONUT_0} />
-                </BudgetCard>
-            </div>
-        );
-    }
-}
-
-export default BudgetCards;
-||||||| merged common ancestors
-=======
 /* --------------------------------------------------------
 author: Dval
 ----------------------------------------------------------*/
@@ -117,4 +57,3 @@ class BudgetCards extends Component {
 }
 
 export default BudgetCards;
->>>>>>> cdffb6465432010e44806591f874f82fa731e8f1
