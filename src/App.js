cd2d854d0a3e2464cd77de29cd3bba6c2f2c8b0e
Dval <ojsboy@hotmail.com>
2018-11-20 15:56:25 +0900

    Add bridge

--- src/App.js
+++ src/App.js
@@ -12,35 +12,49 @@ import BudgetIntro from './components/BudgetIntro';
 import FixedScrollZone from './components/FixedScrollZone';
 import BlockButton from './components/BlockButton';
 import ScrollArrow from './components/ScrollArrow';
-// import * from './lib/bridge';
 import './App.scss';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { osType: OS_TYPE.IOS };
+        this.state = {osType: OsType.IOS, enableButton: true, buttonType: ButtonType.NEXT};
+        window.returnWelcomeButtonType = this.returnWelcomeButtonType.bind(this);
     }
 
     componentDidMount() {
-        const userAgent = navigator.userAgent.toLowerCase();
-        
-        if ((userAgent.search('iphone') > -1) || (userAgent.search('ipod') > -1) || (userAgent.search('ipad') > -1)) {
-            console.log(userAgent);
-            console.log(window.webkit);
-            console.log(window.webkit.messageHandlers);
-            window.webkit.messageHandlers.getButtonType.postMessage("aa");
-        } else if (userAgent.search('android')) {
-            this.setState({ osType: OS_TYPE.ANDROID });
+        try {
+            const userAgent = navigator.userAgent.toLowerCase();
+
+            if (/iphone|ipod|ipad/.test(userAgent)) {
+                window.webkit.messageHandlers.getButtonType.postMessage("");
+            } else if (/android/.test(userAgent)) {
+                this.setState({
+                    osType: OsType.ANDROID,
+                    enableButton: true,
+                    buttonType: window.welcomeView.getButtonType()
+                });
+            }
+        } catch (e) {
+            console.error('Native function call failed.');
         }
 
-        AOS.init({
-            once: true,
-            easing: 'linear'
-        });
+        AOS.init({ once: true, easing: 'linear' });
     }
 
-    returnWelcomeButtonType(type) {
-        console.log(type);
+    closeWebView(buttonType) {
+        const osType = this.state.osType;
+        
+        try {
+            (osType === OsType.IOS) ?
+                window.webkit.messageHandlers.close.postMessage(buttonType) :
+                window.welcomeView.close(buttonType);
+        } catch(e) {
+            throw new Error('Native function call failed.');
+        }                
+    }
+
+    returnWelcomeButtonType(buttonType) {
+        this.setState({...this.state, enableButton: true, buttonType});
     }
 
     render() {
@@ -50,19 +64,37 @@ class App extends Component {
                 <SettingIntro />
                 <OneDayIntro />
                 <BudgetIntro />
-                <FixedScrollZone height={56}>
-                    <BlockButton text="다음에 하기" width="34%" left={0} backgroundColor="#ffb37a" color="white" />
-                    <BlockButton text="시작하기" width="66%" right={0} backgroundColor="white" color="#ffa059" />
-                </FixedScrollZone>
+                {
+                    this.state.enableButton ? 
+                        <FixedScrollZone height={56}>
+                            {
+                                (this.state.buttonType === ButtonType.NEXT) ?
+                                    <BlockButton
+                                        text="다음에 하기"
+                                        width="34%"
+                                        left={0}
+                                        backgroundColor="#ffb37a"
+                                        color="white"
+                                        onClick={() => this.closeWebView(ButtonType.NEXT)}
+                                    /> : ''
+                            }
+                            <BlockButton
+                                text="시작하기"
+                                width={`${(this.state.buttonType === ButtonType.NEXT) ? '66%' : '100%'}`}
+                                right={0}
+                                backgroundColor="white"
+                                color="#ffa059"
+                                onClick={() => this.closeWebView(ButtonType.START)}
+                            />
+                        </FixedScrollZone> : ''
+                }
                 <ScrollArrow />
             </div>
         );
     }
 }
 
-const OS_TYPE = {
-    IOS: 'ios',
-    ANDROID: 'android'
-};
+const OsType = { IOS: 'ios', ANDROID: 'android' };
+const ButtonType = { NEXT: '0', START: '1' };
 
 export default App;
