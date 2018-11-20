2ab3bbf02e19915340804a63d041a514cd830619
Dval <ojsboy@hotmail.com>
2018-11-20 16:46:22 +0900

    Modify ScrollArrow component

--- src/components/ScrollArrow/index.js
+++ src/components/ScrollArrow/index.js
@@ -2,34 +2,9 @@
 author: Dval
 ----------------------------------------------------------*/
 
-import React, { Component } from 'react';
+import React from 'react';
 import './ScrollArrow.scss';
 
-class ScrollArrow extends Component {
-    constructor(props) {
-        super(props);
-        this.handleScroll = this.handleScroll.bind(this);
-    }
-
-    handleScroll() {
-        if (window.scrollY > 20) {
-            window.removeEventListener('scroll', this.handleScroll, false);
-        }
-    }
-
-    componentDidMount() {
-        window.addEventListener('scroll', this.handleScroll, false);
-    }
-
-    componentWillUnmount() {
-        window.addEventListener('scroll', this.handleScroll);
-    }
-
-    render() {
-        return (
-            <div className={`scroll-arrow img-scroll-arrow blink-smooth`}></div>
-        );
-    }
-}
+const ScrollArrow = () => <div className={`scroll-arrow img-scroll-arrow blink-smooth`} />;
 
 export default ScrollArrow;
