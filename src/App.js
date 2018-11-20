697781c6db91dd13c91323d0f8860a3d0898311c
Dval <ojsboy@hotmail.com>
2018-11-20 09:30:48 +0900

    Add os type in App state

--- src/App.js
+++ src/App.js
@@ -11,14 +11,36 @@ import OneDayIntro from './components/OneDayIntro';
 import BudgetIntro from './components/BudgetIntro';
 import FixedScrollZone from './components/FixedScrollZone';
 import BlockButton from './components/BlockButton';
+import ScrollArrow from './components/ScrollArrow';
+// import * from './lib/bridge';
 import './App.scss';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { osType: OS_TYPE.IOS };
+    }
+
     componentDidMount() {
+        const userAgent = navigator.userAgent.toLowerCase();
+        
+        if ((userAgent.search('iphone') > -1) || (userAgent.search('ipod') > -1) || (userAgent.search('ipad') > -1)) {
+            console.log(userAgent);
+            console.log(window.webkit);
+            console.log(window.webkit.messageHandlers);
+            window.webkit.messageHandlers.getButtonType.postMessage("aa");
+        } else if (userAgent.search('android')) {
+            this.setState({ osType: OS_TYPE.ANDROID });
+        }
+
         AOS.init({
             once: true,
             easing: 'linear'
         });
+    }
+
+    returnWelcomeButtonType(type) {
+        console.log(type);
     }
 
     render() {
@@ -32,9 +54,15 @@ class App extends Component {
                     <BlockButton text="다음에 하기" width="34%" left={0} backgroundColor="#ffb37a" color="white" />
                     <BlockButton text="시작하기" width="66%" right={0} backgroundColor="white" color="#ffa059" />
                 </FixedScrollZone>
+                <ScrollArrow />
             </div>
         );
     }
 }
+
+const OS_TYPE = {
+    IOS: 'ios',
+    ANDROID: 'android'
+};
 
 export default App;
